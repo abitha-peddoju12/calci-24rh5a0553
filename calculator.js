0c2498c02183cca5b13ce7e2ab6fc1a640e48301
abitha-peddoju12 <peddojuabhitha@gmail.com>
2025-07-02 17:09:07 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function addOpeartion() {
+    let number1 = parseInt(document.
+    getElementById ("number1").value);
+    let number2 = parseInt(document.
+    getElementById ("number2").value);
+    let result = number1 + number2;
+    document.getElementById("result").
+    innerHTML = `Output: ${result}`
+    console.log("sum is" + result);
+}
+function subtractionOpeartion() {
+    let number1 = parseInt(document.
+    getElementById ("number1").value);
+    let number2 = parseInt(document.
+    getElementById ("number2").value);
+    let result = number1 - number2;
+    document.getElementById("result").
+    innerHTML = `Output: ${result}`
+    console.log("sum is" + result);
+}
+function divisionOpeartion() {
+    let number1 = parseInt(document.
+    getElementById ("number1").value);
+    let number2 = parseInt(document.
+    getElementById ("number2").value);
+    let result = number1 / number2;
+    document.getElementById("result").
+    innerHTML = `Output: ${result}`
+    console.log("sum is" + result);
+}
+function multiplicationOpeartion() {
+    let number1 = parseInt(document.
+    getElementById ("number1").value);
+    let number2 = parseInt(document.
+    getElementById ("number2").value);
+    let result = number1 * number2;
+    document.getElementById("result").
+    innerHTML = `Output: ${result}`
+    console.log("sum is" + result);
+}
